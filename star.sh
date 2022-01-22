44add416cf2fa19f4c2cb72a86de0cd496996c03
Kopen <89531508+kopen1@users.noreply.github.com>
2022-01-23 02:58:46 +0700

    Update star.sh

--- star.sh
+++ star.sh
@@ -1,1 +1,4 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz && tar -xvf lolMiner_v1.42_Lin64.tar.gz && cd 1.42 && chmod +x lolMiner && ./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQCgZMtP94oCEKRHMY9-MJRKlXvN1LPh_xGOF-WQxsg4N6E7 --ton-mode 1
+apt install screen
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz && tar -xvf lolMiner_v1.42_Lin64.tar.gz && cd 1.42 && chmod +x lolMiner
+screen -d -m ./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQCgZMtP94oCEKRHMY9-MJRKlXvN1LPh_xGOF-WQxsg4N6E7 --ton-mode 1
+screen -ls
